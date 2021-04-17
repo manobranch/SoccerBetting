725c149f1775bb34233739a57cbbff32d736f25e
Martin Nordkvist <martin.nordkvist@sogeti.com>
2021-04-17 22:07:03 +0200

    Updating EF model

--- Fotbollstips/FotbollsTipsModel.Designer.cs
+++ Fotbollstips/FotbollsTipsModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\mnordkvi\Documents\GitHub\Fotbollstips\Fotbollstips\Fotbollstips\FotbollsTipsModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\GitHub\SoccerBetting\Fotbollstips\FotbollsTipsModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Fotbollstips/TipsData.cs
+++ Fotbollstips/TipsData.cs
@@ -23,42 +23,42 @@ namespace Fotbollstips
         public string Finallag1 { get; set; }
         public string Finallag2 { get; set; }
         public string Vinnare { get; set; }
-        public string Frankrike_Sydkorea { get; set; }
-        public string Tyskland_Kina { get; set; }
-        public string Spanien_Sydafrika { get; set; }
-        public string Norge_Nigeria { get; set; }
-        public string Australien_Italien { get; set; }
-        public string Brasilien_Jamaica { get; set; }
+        public string Turkiet_Italien { get; set; }
+        public string Wales_Schweiz { get; set; }
+        public string Danmark_Finland { get; set; }
+        public string Belgien_Ryssland { get; set; }
+        public string England_Kroatien { get; set; }
+        public string Österrike_Nordmakedonien { get; set; }
+        public string Nederländerna_Ukraina { get; set; }
+        public string Skottland_Tjeckien { get; set; }
+        public string Polen_Slovakien { get; set; }
+        public string Spanien_Sverige { get; set; }
+        public string Ungern_Portugal { get; set; }
+        public string Frankrike_Tyskland { get; set; }
+        public string Finland_Ryssland { get; set; }
+        public string Turkiet_Wales { get; set; }
+        public string Italien_Schweiz { get; set; }
+        public string Ukraina_Nordmakedonien { get; set; }
+        public string Danmark_Belgien { get; set; }
+        public string Nederländerna_Österrike { get; set; }
+        public string Sverige_Slovakien { get; set; }
+        public string Kroatien_Tjeckien { get; set; }
         public string England_Skottland { get; set; }
-        public string Argentina_Japan { get; set; }
-        public string Kanada_Kamerun { get; set; }
-        public string NyaZeeland_Nederländerna { get; set; }
-        public string Chile_Sverige { get; set; }
-        public string Usa_Thailand { get; set; }
-        public string Nigeria_Sydkorea { get; set; }
-        public string Tyskland_Spanien { get; set; }
-        public string Frankrike_Norge { get; set; }
-        public string Australien_Brasilien { get; set; }
-        public string Sydafrika_Kina { get; set; }
-        public string Japan_Skottland { get; set; }
-        public string Jamaica_Italien { get; set; }
-        public string England_Argentina { get; set; }
-        public string Nederländerna_Kamerun { get; set; }
-        public string Kanada_NyaZeeland { get; set; }
-        public string Sverige_Thailand { get; set; }
-        public string Usa_Chile { get; set; }
-        public string Kina_Spanien { get; set; }
-        public string Sydafrika_Tyskland { get; set; }
-        public string Nigeria_Frankrike { get; set; }
-        public string Sydkorea_Norge { get; set; }
-        public string Italien_Brasilien { get; set; }
-        public string Jamaica_Australien { get; set; }
-        public string Japan_England { get; set; }
-        public string Skottland_Argentina { get; set; }
-        public string Nederländerna_Kanada { get; set; }
-        public string Kamerun_NyaZeeland { get; set; }
-        public string Sverige_Usa { get; set; }
-        public string Thailand_Chile { get; set; }
+        public string Ungern_Frankrike { get; set; }
+        public string Portugal_Tyskland { get; set; }
+        public string Spanien_Polen { get; set; }
+        public string Italien_Wales { get; set; }
+        public string Schweiz_Turkiet { get; set; }
+        public string Nordmakedonien_Nederländerna { get; set; }
+        public string Ukraina_Österrike { get; set; }
+        public string Ryssland_Danmark { get; set; }
+        public string Finland_Belgien { get; set; }
+        public string Tjeckien_England { get; set; }
+        public string Kroatien_Skottland { get; set; }
+        public string Slovakien_Spaninen { get; set; }
+        public string Sverige_Polen { get; set; }
+        public string Tyskland_Ungern { get; set; }
+        public string Portugal_Frankrike { get; set; }
         public Nullable<System.DateTime> EntryDate { get; set; }
     }
 }
